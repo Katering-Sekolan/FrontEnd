09b05dfd7b0a36a69aa3ff6942c059ccbdd54268
wahyuwardana2 <wahyu.wardana100@gmail.com>
2023-11-18 03:03:41 +0700

    Add : pelanggan >> crud

--- pages/admins/pelanggan.jsx
+++ pages/admins/pelanggan.jsx
@@ -1,102 +1,212 @@
-import React, { useState } from 'react';
-import { Box, Button, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
 import Header from "@/components/Header";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+import SaveIcon from "@mui/icons-material/Save";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import axios from "axios";
 import theme from "@/config/theme";
+import SweatAlertTimer from "@/config/SweatAlert/timer";
 
-function Pelanggan() {
-    const [pelangganList, setPelangganList] = useState([]);
-    const [namaPelanggan, setNamaPelanggan] = useState('');
-    const [nomorHP, setNomorHP] = useState('');
+export default function Pelanggan() {
+  const [pelanggan, setPelanggan] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [newPelanggan, setNewPelanggan] = useState({ nohp: "", nama: "" });
+  const [editingPelanggan, setEditingPelanggan] = useState(null);
 
-    const handleTambahPelanggan = () => {
-        // Validasi form
-        if (!namaPelanggan || !nomorHP) {
-            alert("Nama dan Nomor HP pelanggan harus diisi.");
-            return;
-        }
-
-        // Logic untuk menambah pelanggan
-        const newPelanggan = {
-            id: pelangganList.length + 1,
-            nama: namaPelanggan,
-            nomorHP: nomorHP
-        };
-
-        setPelangganList([...pelangganList, newPelanggan]);
-        // Reset nilai namaPelanggan dan nomorHP setelah menambah pelanggan
-        setNamaPelanggan('');
-        setNomorHP('');
-    };
-
-    const handleHapusPelanggan = (id) => {
-        // Logic untuk menghapus pelanggan berdasarkan id
-        const filteredPelanggan = pelangganList.filter((pelanggan) => pelanggan.id !== id);
-        setPelangganList(filteredPelanggan);
-    };
-
-    return (
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "nohp", headerName: "No HP", width: 250 },
+    { field: "nama", headerName: "Nama", width: 250 },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 250,
+      renderCell: (params) => (
         <>
-          <ThemeProvider theme={theme}>
-      <Box sx={{ display: "flex" }}>
-        <Header navName="Pelanggan Katering Qita" />
-            <Box sx={{ p: 3 }}>
-                <Typography variant="h5" component="h2" mb={2}>
-                    Daftar Pelanggan
-                </Typography>
-                <TableContainer  component={Paper}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>ID</TableCell>
-                                <TableCell>Nama Pelanggan</TableCell>
-                                <TableCell>Nomor HP</TableCell>
-                                <TableCell>Action</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {pelangganList.map((pelanggan) => (
-                                <TableRow key={pelanggan.id}>
-                                    <TableCell>{pelanggan.id}</TableCell>
-                                    <TableCell>{pelanggan.nama}</TableCell>
-                                    <TableCell>{pelanggan.nomorHP}</TableCell>
-                                    <TableCell>
-                                        <Button variant="outlined" color="secondary" onClick={() => handleHapusPelanggan(pelanggan.id)}>
-                                            Hapus
-                                        </Button>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <Typography variant="h6" component="h2" mt={2}>
-                    Tambah Pelanggan
-                </Typography>
-                <TextField
-                    label="Nama Pelanggan"
-                    variant="outlined"
-                    value={namaPelanggan}
-                    onChange={(event) => setNamaPelanggan(event.target.value)}
-                    margin="normal"
-                    fullWidth
-                />
-                <TextField
-                    label="Nomor HP"
-                    variant="outlined"
-                    value={nomorHP}
-                    onChange={(event) => setNomorHP(event.target.value)}
-                    margin="normal"
-                    fullWidth
-                />
-                <Button variant="contained" color="secondary" onClick={handleTambahPelanggan} mt={2}>
-                    Tambah Pelanggan
-                </Button>
-            </Box>
-            </Box>
-            </ThemeProvider>
+          <Button
+            variant="outlined"
+            size="small"
+            startIcon={<EditIcon />}
+            onClick={() => handleEditPelanggan(params.row.id)}
+            sx={{ marginRight: 1 }}
+          >
+            Edit
+          </Button>
+          <Button
+            variant="outlined"
+            size="small"
+            startIcon={<DeleteIcon />}
+            onClick={() => handleDeletePelanggan(params.row.id)}
+          >
+            Delete
+          </Button>
         </>
-    );
-}
+      ),
+    },
+  ];
 
-export default Pelanggan;
+  useEffect(() => {
+    fetchPelanggan();
+  }, []);
+
+  const fetchPelanggan = async () => {
+    try {
+      const response = await axios.get(
+        process.env.NEXT_PUBLIC_API_URL + "/u/get-all"
+      );
+      // Menambahkan properti 'id' ke setiap objek pelanggan
+      const pelangganWithId = response.data.data.map((pelanggan) => ({
+        ...pelanggan,
+        id: pelanggan._id,
+      }));
+      setPelanggan(pelangganWithId);
+    } catch (error) {
+      console.error("Error fetching pelanggan:", error);
+    }
+  };
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+    setEditingPelanggan(null);
+    setNewPelanggan({ nohp: "", nama: "" });
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewPelanggan({ ...newPelanggan, [name]: value });
+  };
+
+  const handleAddPelanggan = async () => {
+    try {
+      await axios.post(
+        process.env.NEXT_PUBLIC_API_URL + "/u/create",
+        newPelanggan
+      );
+      fetchPelanggan();
+      handleCloseModal();
+      SweatAlertTimer("Pelanggan Ditambahkan!", "success");
+    } catch (error) {
+      console.error("Error adding pelanggan:", error);
+      SweatAlertTimer("Error!", "Gagal menambahkan pelanggan", "error");
+    }
+  };
+
+  const handleUpdatePelanggan = async () => {
+    try {
+      await axios.put(
+        process.env.NEXT_PUBLIC_API_URL + `/u/update/${editingPelanggan}`,
+        newPelanggan
+      );
+      fetchPelanggan();
+      handleCloseModal();
+      SweatAlertTimer("Pelanggan Diupdate!", "success");
+    } catch (error) {
+      console.error("Error updating pelanggan:", error);
+      SweatAlertTimer("Error!", "Gagal mengupdate pelanggan", "error");
+    }
+  };
+
+  const handleEditPelanggan = (pelangganId) => {
+    const pelangganToEdit = pelanggan.find(
+      (pelanggan) => pelanggan.id === pelangganId
+    );
+    setEditingPelanggan(pelangganId);
+    setNewPelanggan({ ...pelangganToEdit });
+    setOpenModal(true);
+  };
+
+  const handleDeletePelanggan = async (pelangganId) => {
+    try {
+      await axios.delete(
+        process.env.NEXT_PUBLIC_API_URL + `/u/delete/${pelangganId}`
+      );
+      fetchPelanggan();
+      handleCloseModal();
+      SweatAlertTimer("Pelanggan Dihapus!", "success");
+    } catch (error) {
+      console.error("Error deleting pelanggan:", error);
+      SweatAlertTimer("Error!", "Gagal menghapus pelanggan", "error");
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <Header navName="Pelanggan Katering Qita" />
+        <Box
+          sx={{
+            marginTop: 8,
+            flexGrow: 1,
+            padding: 2,
+          }}
+        >
+          <Button variant="contained" onClick={handleOpenModal}>
+            Tambah Pelanggan
+          </Button>
+
+          <DataGrid rows={pelanggan} columns={columns} pageSize={5} />
+
+          <Modal open={openModal} onClose={handleCloseModal}>
+            <Box
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                bgcolor: "white",
+                boxShadow: 24,
+                p: 4,
+              }}
+            >
+              <h2>
+                {editingPelanggan ? "Edit Pelanggan" : "Tambah Pelanggan"}
+              </h2>
+              <div>
+                <TextField
+                  label="Nama"
+                  name="nama"
+                  value={newPelanggan.nama}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="No HP"
+                  name="nohp"
+                  value={newPelanggan.nohp}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <Button
+                  variant="contained"
+                  onClick={
+                    editingPelanggan
+                      ? handleUpdatePelanggan
+                      : handleAddPelanggan
+                  }
+                  startIcon={<SaveIcon />}
+                  fullWidth
+                >
+                  {editingPelanggan ? "Update" : "Simpan"}
+                </Button>
+              </div>
+            </Box>
+          </Modal>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
