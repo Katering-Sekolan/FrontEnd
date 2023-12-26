64a1a595376bff97cee2d5c707d16ff578b303e5
setyarp <setyarizkypradana@gmail.com>
2023-12-26 11:22:15 +0700

    update: response time, logout not fix

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -5,4 +5,12 @@ export const WhatsAppService = {
     const response = await API.post("/wa/sendMessage", data);
     return response;
   },
+  broadcastMessages: async () => {
+    const response = await API.post("/wa/broadcastMessage");
+    return response;
+  },
+  logout: async () => {
+    const response = await API.post("/wa/logout");
+    return response;
+  },
 };
